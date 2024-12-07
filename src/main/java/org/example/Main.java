0c4d401398e112605fa97950d38ed6c05dad3741
Andriy Plaksenkov <plaksenkov.andriy@lll.kpi.ua>
2024-12-07 23:29:00 +0200

    filtered by parameter `Б` and gathered by parameter `B`

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,10 @@ import org.example.domain.ClothingItem;
 import org.example.gatherer.ClothingGatherer;
 import org.example.generator.ClothingItemGenerator;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 public class Main {
@@ -18,6 +21,13 @@ public class Main {
                 .toList();
         for (ClothingItem clothingItem : clothingItems) {
             System.out.println(clothingItem);
+        }
+
+        Map<String, List<ClothingItem>> groupedClothingItems = clothingItems.stream()
+                .filter(item -> item.getProductionDate().isAfter(LocalDate.now().minusMonths(240)))
+                .collect(Collectors.groupingBy(ClothingItem::getFabricType));
+        for (Map.Entry<String, List<ClothingItem>> entry : groupedClothingItems.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue().size());
         }
     }
 }
